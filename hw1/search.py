fc3abd820cdf5d8ee817efa74f7d43403f88a88a
amirandarabi <andarabi.amirhosein@gmail.com>
2018-11-01 14:16:23 +0330

    it's works but autograder don't let use manhattanDistance function from util so i take another way!!

--- hw1/search.py
+++ hw1/search.py
@@ -96,148 +96,7 @@ def depthFirstSearch(problem):
 
     """
     "*** YOUR CODE HERE ***"
-    # this part is my trying to understand how it's works
-    # print "Start:", problem.getStartState()
-    # print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    # print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    # startstate = problem.getStartState()
-    # openlist = util.Stack()
-    # openlist.push(problem.getStartState())
-    # patch =[]
-    # # n = openlist.pop()
-    # # for i in problem.getSuccessors(n):
-    # #     openlist.push(i[0])
-    # # n = openlist.pop()
-    # # print n
-    # # s = problem.getSuccessors(n)
-    # # print s
-    # closelist = util.Stack()
-    # x = 1500
-    # # while openlist.isEmpty() is not True:
-    # while x is not 0:
-    #     print "hear"
-    #     x -= 1
-    #     n = openlist.pop()
-    #
-    #     if problem.isGoalState(n):
-    #         return patch
-    #     else:
-    #         closelist.push(n)
-    #         for i in problem.getSuccessors(n):
-    #             openlist.push(i[0])
-    #             # return [i[1]]
-    #
-    # # print openlist.pop()
-    # # print openlist.pop()
-    # # print "pacman State is:", problem.isWin(problem.getStartState())
-    # return ["West"]
-    # initial freng, close lists and the patch of goal
-    # open = util.Stack()
-    # close = []
-    # patch = []
-    # # getStartState
-    # startState = problem.getStartState()
-    # # initial a flage that show we arrive to goal or not
-    # flag = False
-    # i = 100
-    # while flag is False and i is not 0:
-    #     i -= 1
-    #     if open.isEmpty() and len(close) is 0:
-    #         open.push([startState, "root", 0])
-    #         patch.append([startState, "root", 0])
-    #     cs = open.pop()
-    #     # print cs[1]
-    #     lp = patch.pop()
-    #     # print lp[1]
-    #     if util.manhattanDistance(lp[0], cs[0]) == 1:
-    #         patch.append(lp)
-    #         patch.append(cs)
-    #     else:
-    #         patch.append(cs)
-    #     # print patch
-    #     if problem.isGoalState(cs[0]):
-    #         # patch.append(cs)
-    #         flag = True
-    #     else:
-    #         close.append(cs)
-    #         print "close1", close
-    #         children = problem.getSuccessors(cs[0])
-    #         print "chidren", children
-    #         children.reverse()
-    #         print "children", children
-    #         for child in children:
-    #             if util.manhattanDistance(child[0], cs[0]) == 2:
-    #                 continue
-    #             # if child[1] == "West" and cs[1] == "East":
-    #             #     continue
-    #             # if child[1] == "East" and cs[1] == "West":
-    #             #     continue
-    #             # if child[1] == "South" and cs[1] == "North":
-    #             #     continue
-    #             # if child[1] == "North" and cs[1] == "South":
-    #             #     continue
-    #             # print close
-    #             if isdead(child, close):
-    #                 print 1
-    #                 continue
-    #             # else:
-    #             #     open.push(child)
-    #
-    #             # print "close", close
-    #             print 2
-    #             open.push(child)
 
-
-    # patch = ["South", "South", "West", "West", "West", "West", "South", "South",
-    #  "East", "East", "East", "East", "South", "South", "West", "West", "West",
-    #   "West", "South", "South", "East", "East", "East", "East", "South", "South",
-    #    "West", "West", "West", "West", "South", "South", "East", "East", "East",
-    #    "East", "South", "South", "West", "West", "West","South","South", "South"]
-    # def checkfamily()
-    # open.push(startState)
-    # while open.isEmpty() is False:
-    #     cs = open.pop()
-    #     if len(patch) is 0:
-    #         x = []
-    #         x.append(cs)
-    #         patch.append(x)
-    #     lp = patch.pop()
-    #     # print lp[1]
-    #     if util.manhattanDistance(lp[0], cs[0]) == 1:
-    #         patch.append(lp)
-    #         patch.append(cs)
-    #     else:
-    #         patch.append(cs)
-    #     if problem.isGoalState(cs):
-    #         patch2 = []
-    #         for p in patch:
-    #             patch2.append(p[1])
-    #         return patch2
-    #     else:
-    #         close.append(cs)
-    #         children = problem.getSuccessors(cs)
-    #         for child in children:
-    #             flag = False
-    #             for c in close:
-    #                 if c[0] != child[0]:
-    #                     flag = True
-    #             if flag:
-    #                 open.push(child)
-
-
-
-    # patch2 = []
-    # for p in patch:
-    #     patch2.append(p[1])
-    # #     patch.remove(p)
-    # #     while p[0][0]:
-    # #         pass
-    # patch2.remove(patch[0][1])
-    # # patch2.remove('e')
-    # print "hear"
-    # # print close
-    # print patch2
-    # return patch2
 
 
     # start again !!
@@ -253,10 +112,17 @@ def depthFirstSearch(problem):
         cs = openlist.pop()
         #this part produce the patch
         if cs[1] != "root":
-            lp =patch.pop()
-            # this while remove extra nodes in patch by calculate manhattan Distance 
-            while util.manhattanDistance(lp[0],cs[0]) != 1:
+            # lp =patch.pop()
+            # this while remove extra nodes in patch by calculate manhattan Distance
+            # while util.manhattanDistance(lp[0],cs[0]) != 1:
+            #     lp =patch.pop()
+            flag = True
+            while flag:
                 lp =patch.pop()
+                for ch in problem.getSuccessors(lp[0]):
+                    if ch[0] == cs[0]:
+                        flag = False
+                        break
             patch.append(lp)
             patch.append(cs)
         else:
@@ -268,12 +134,12 @@ def depthFirstSearch(problem):
             for p in patch:
                 answerpatch.append(p[1])
             answerpatch.remove(patch[0][1])
-            print patch
+            # print patch
             return answerpatch
         else:
             closelist.append(cs)
             children = problem.getSuccessors(cs[0])
-            children.reverse()
+            # children.reverse()
             for child in children:
                 flag = False
                 for c in closelist:
@@ -290,11 +156,13 @@ def depthFirstSearch(problem):
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
+    return []
     util.raiseNotDefined()
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
+    return []
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
@@ -307,6 +175,7 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    return []
     util.raiseNotDefined()
 
 
